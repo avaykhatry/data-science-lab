14f55f97d80cba82032d4be23646525abc5657dd
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-04 20:53:42 +0545

    feat: add datasets

---
+++ 01-data-inspection/main.py
@@ -0,0 +1,13 @@
+#.   Done by Avaya Khatri
+##.  4
+###. Warming Up Exercise
+
+# Problem 1 - Data read, write and inspect
+# Working with bank.csv
+
+import pandas as pd
+
+dataset1 = pd.read_csv('bank.csv')
+
+
+
